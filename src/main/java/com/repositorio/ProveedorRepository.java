81da7a8f9645b1ec2614727842cbc3250eaf50a0
carlos033 <cdr8725@gmail.com>
2023-03-07 17:52:44 +0100

    correccion Query

--- src/main/java/com/repositorio/ProveedorRepository.java
+++ src/main/java/com/repositorio/ProveedorRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 
 import com.entidad.Proveedor;
 
+@EnableJpaRepositories
 public interface ProveedorRepository extends JpaRepository<Proveedor, Integer> {
 	@Query(value = "Select p FROM Proveedor p WHERE p.idCliente =: idCliente")
-	List<Proveedor> findProveedoresByIdCliente(int idCliente);
+	List<Proveedor> findProveedoresByIdCliente(@Param("idCliente") int idCliente);
 }
